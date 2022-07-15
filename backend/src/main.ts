dd6bde359c36c4d713110e9cfe7a47e55cd1cb47
ms.equilibrium <simunovic.marko@outlook.com>
2022-07-15 11:48:05 +0200

    Using env port if available

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3043;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: 'http://localhost:5528' });
-  await app.listen(3043);
+  await app.listen(port);
 }
 bootstrap();
