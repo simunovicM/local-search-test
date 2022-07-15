9e706192bf305b123dbe7e3709a1f679b058aa0f
ms.equilibrium <simunovic.marko@outlook.com>
2022-07-15 11:41:46 +0200

    Added heroku setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { BusinessesModule } from './businesses/businesses.module';
 
 @Module({
-  imports: [BusinessesModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'frontend/dist'),
+    }),
+    BusinessesModule],
   controllers: [],
   providers: [],
 })
