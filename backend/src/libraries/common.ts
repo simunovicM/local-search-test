c3357fcac429469518d0c5826cd8a52a658ea585
ms.equilibrium <simunovic.marko@outlook.com>
2022-07-17 11:56:37 +0200

    Fixing propToFnc definition

--- backend/src/libraries/common.ts
+++ backend/src/libraries/common.ts
@@ -1,6 +1,6 @@
 /* istanbul ignore file */
 export const isFunction = (value): boolean => typeof value === 'function';
-export const propToFnc: (item) => any = (prop?: ((item) => any) | string): ((item) => any) =>
+export const propToFnc: ((prop?: ((item) => any) | string) => ((item, ind?: number) => any)) = (prop?) =>
   isFunction(prop)
     ? (prop as (item) => any)
     : (f) => (prop ? f[prop as string] : f);
